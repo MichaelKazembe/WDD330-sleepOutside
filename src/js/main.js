8ccdb4f7bec76a251450e7bf4c3c0d6a361031c1
MichaelKazembe <michaelkazembe@gmail.com>
2025-09-23 18:59:01 +0200

    Refactor main.js by removing unused alert system and product rendering code

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,5 @@
-// main.js
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import Alert from "../js/alert.js";
-// Get all products
-const productListData = dataSource.getProducts();
-// Render the products on the page
-productList.renderList(productListData);
-
-// Load alerts from JSON
-const alertSystem = new Alert("../json/alert.json");
-alertSystem.loadAlerts();
-
-loadHeaderFooter();
-
-if (document.querySelector("#product-list")) {
-  const products = new ProductList("tents", "#product-list");
-  products.init();
-}
 
 const dataSource = new ProductData("tents");
 const category = "tents";
@@ -24,3 +7,5 @@ const listElement = document.querySelector(".product-list");
 
 const productList = new ProductList(category, dataSource, listElement);
 productList.init();
+
+
