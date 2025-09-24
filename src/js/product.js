e789359e3de1082f267be23e15a0e0ee3146dd89
MichaelKazembe <michaelkazembe@gmail.com>
2025-09-24 16:12:03 +0200

    Fix import path for loadHeaderFooter and update page title in index.html

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,7 @@
 import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-import { loadHeaderFooter } from "../utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
