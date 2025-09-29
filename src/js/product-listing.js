b55c1d79018a1e85c87eca4a9da948514bb2518d
MichaelKazembe <michaelkazembe@gmail.com>
2025-09-29 15:06:29 +0200

    Rename ProductData to ExternalServices in relevant files and update imports accordingly

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,11 +1,11 @@
-import ProductData from "./ExternalServices.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter, getParam } from "./utils.mjs";
 
 loadHeaderFooter();
 
 const category = getParam("category");
-const dataSource = new ProductData();
+const dataSource = new ExternalServices();
 const listElement = document.querySelector(".product-list");
 
 // Set the page title to include the category
